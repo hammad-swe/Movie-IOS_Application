a6da562839172e0618bf8cf6263fb5a4ecfd7d9a
Hammad Ali <hammadali78056@gmail.com>
2026-02-23 09:36:01 +0500

    Feat: Make Title a SwiftData @Model and add modelContainer
    Integrates SwiftData persistence: imports SwiftData, converts Title from a Decodable struct to an @Model class, adds an initializer and a custom Decodable implementation (CodingKeys) to map API fields, and registers the model container in the App (.modelContainer(for: Title.self)). This prepares the Title type for SwiftData storage while keeping existing preview data.

--- Movie-ios-app/Movie_ios_appApp.swift
+++ Movie-ios-app/Movie_ios_appApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct Movie_ios_appApp: App {
@@ -13,5 +14,6 @@ struct Movie_ios_appApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for : Title.self)
     }
 }
